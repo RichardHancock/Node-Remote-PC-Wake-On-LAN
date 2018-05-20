d03e5d34bdb752c9d8ef1d2d72ce8325d99c08e6
Richard Hancock <me@richardhancock.co.uk>
2018-05-20 22:25:05 +0100

    Added Test Xbox Functionality
    Apparently added this but never commited.
    Commiting due to computer reformat.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var wakeOnLAN = require('node-wol');
+var xboxOn = require('xbox-on');
 var app = express();
 var http = require('http').Server(app);
 
@@ -8,7 +9,6 @@ app.use(bodyParser.json());
 
 app.post('/pwr', function(req, res)
 {
-	console.log("Ping");
 	console.log(req.body);
 
 	if (req.body.passphrase === "m&Q8!!CsquGpA&E!%zFW6ZfW&mBkDVUU")
@@ -19,6 +19,47 @@ app.post('/pwr', function(req, res)
 		    return;
 		  }
 		});
+	}
+
+	res.sendStatus(200);
+});
+
+app.post('/sam', function(req, res)
+{
+	console.log(req.body);
+
+	if (req.body.passphrase === "!6Ynb6z*b6J#gN%an9dt2eNVKDt5@G$5")
+	{
+		wakeOnLAN.wake('40:16:3B:C7:72:48', function(error) {
+		  if(error) {
+		    console.log(error);
+		    return;
+		  }
+		});
+	}
+
+	res.sendStatus(200);
+});
+
+app.post('/xbx', function(req, res)
+{
+	console.log(req.body);
+
+	if (req.body.passphrase === "ZD3U55GX*qjB!TP^k5TNCAqk6*!C#KFq")
+	{
+		var ipAddress = "192.168.1.36";
+		var liveDeviceId = "FD004B1F8AEFD5FA";
+
+		var xbox = new xboxOn(ipAddress, liveDeviceId); // Create new xbox
+
+		// Optional, defaults shown
+		var options = {
+		    tries: 5,
+		    delay: 1000,
+		    waitForCallback: false
+		};
+
+		xbox.powerOn(options);
 	}
 
 	res.sendStatus(200);
